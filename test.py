9c0454a33e53396136edc0876d7d3b170c2ed0c4
xhf3571 <951988533@qq.com>
2025-08-20 21:50:44 +0800

    1

--- test.py
+++ test.py
@@ -362,7 +362,7 @@ def test(data,
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='test.py')
-    parser.add_argument('--weights', nargs='+', type=str, default='/home/disk/xuhaifeng/yolov5-pose/runs/train/yolov5n_kpts-crowdpose14/weights/best.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='/home/disk/xuhaifeng/yolov5-pose/runs/train/yolov5s_kpts-crowdpose/weights/best.pt', help='model.pt path(s)')
     parser.add_argument('--data', type=str, default='data/crowdpose.yaml', help='*.data path')
     parser.add_argument('--batch-size', type=int, default=32, help='size of each image batch')
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
